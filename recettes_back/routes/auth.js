d4a9b0a860ddcb9a15dbd799eecdf28c22dac6fe
AIT HOUSSA Yassmine <yassmine.aithoussa.41@edu.uiz.ac.ma>
2025-11-12 01:28:28 +0100

    sauvegarde avant rebase

--- recettes_back/routes/auth.js
+++ recettes_back/routes/auth.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
 import express from 'express';
 import { register, login, getMe } from '../controllers/authController.js';
 import { protect } from '../middleware/auth.js';
+
+// Création d’un routeur Express
 
 const router = express.Router();
 
@@ -11,17 +12,3 @@ router.get('/me', protect, getMe);
 
 export default router;
 
-=======
-import express from 'express';
-import { register, login, getMe } from '../controllers/authController.js';
-import { protect } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', protect, getMe);
-
-export default router;
-
->>>>>>> 7bcff48 (add au projet recettes)
